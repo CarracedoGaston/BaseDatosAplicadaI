f92715a3d45b6dd450f3ab171bb0ca07b2a597ab
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-10-17 08:49:35 -0300

    indentation correction

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const mongoDBURL =
 mongoose.connect(mongoDBURL, {useNewUrlParser: true})
 
 let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', "*");
+    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Headers', "*")
     next();
 }
 
-app.use(allowCrossDomain);
+app.use(allowCrossDomain)
 
 app.use(express.static('public'))
 

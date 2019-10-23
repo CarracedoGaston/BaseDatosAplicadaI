7f25abb87f56d2fe511df9c9163790bcec7204f8
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-10-22 23:02:25 -0300

    heroku script

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const router = require('./router')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const mongoDBURL = 
 'mongodb+srv://Gaston:1234@clusterdb-1esq3.mongodb.net/escribania'
@@ -18,7 +18,16 @@ let allowCrossDomain = function(req, res, next) {
 
 app.use(allowCrossDomain)
 
+// if( process.env.NODE_ENV === 'production') {
+//     app.use(express.static('public'))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "index.html"))
+//     })
+// }
+
 app.use(express.static('public'))
+app.get('*')
+
 
 app.use('/api', router)
 

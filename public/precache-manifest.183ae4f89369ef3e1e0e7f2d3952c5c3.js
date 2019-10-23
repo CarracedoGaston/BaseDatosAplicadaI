b9d1558be481e0420c8ff93f1da44796e803fa61
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-10-22 22:23:27 -0300

    Front build

---
+++ public/precache-manifest.183ae4f89369ef3e1e0e7f2d3952c5c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f9ef885505f0b6524b23ae7e85534f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27e9ed6b508678d3897c",
+    "url": "/static/css/main.7c4bcf05.chunk.css"
+  },
+  {
+    "revision": "0887d18a2440045da0d8",
+    "url": "/static/js/2.ca9a1ee2.chunk.js"
+  },
+  {
+    "revision": "27e9ed6b508678d3897c",
+    "url": "/static/js/main.58156b80.chunk.js"
+  },
+  {
+    "revision": "49409fabaa0fc46adaf4",
+    "url": "/static/js/runtime-main.45352b2d.js"
+  }
+]);
